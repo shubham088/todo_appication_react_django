cb21d02d27a7228e8293d914b145e27e06636647
shubham088 <shubhamsharma0296@gmail.com>
2021-03-19 13:44:03 +0530

    modified files to incorporate redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import HomePage from './components/homePage'
+import HomePageWithRedux from './components/homePageRedux'
 import UpdateTodoComponent from './components/UpdateTodo'
 import CreateTodoComponent from './components/createTodo'
 import {Route, Switch } from 'react-router-dom';
@@ -11,6 +12,7 @@ function App() {
         <div className="App">
           <header className="App-header">
           <Route exact path='/' component={HomePage} />
+          <Route exact path='/home-redux' component={HomePageWithRedux} />
           <Route exact path = '/edit/:id' component = {UpdateTodoComponent} /> 
           <Route exact path = '/create_todo' component = {CreateTodoComponent} /> 
           </header>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,11 +7,15 @@ import { BrowserRouter} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 // Put any other imports below so that CSS from your
 // components takes precedence over default styles.
+import { Provider } from 'react-redux';
+import store  from './store/index.js';
 ReactDOM.render(
   
     <BrowserRouter>
+      <Provider store = {store}>
       <App />
-    </BrowserRouter>
+      </Provider>
+    </BrowserRouter >
   ,
   document.getElementById('root')
 );
